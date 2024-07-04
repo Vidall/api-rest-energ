0dc6cc5c53c5c75197480eb62d4d07d4746ccd32
luan vidal <luanvidal6565@outlook.com>
2024-07-04 17:52:45 -0300

    feat: controller deleteById pessoaFisica

--- src/server/controllers/clients/pessoaFisica/index.ts
+++ src/server/controllers/clients/pessoaFisica/index.ts
@@ -1,9 +1,13 @@
 import * as create from './Create';
 import * as UpdateById from './UpdateById';
 import * as getById from './GetById';
+import * as getAll from './getAll';
+import * as deleteById from './DeleteById';
 
 export const pessoaFisicaControllers = {
   ...create,
   ...UpdateById,
-  ...getById
+  ...getById,
+  ...getAll,
+  ...deleteById
 };
--- src/server/routes/clients/pessoaFisicaRoute.ts
+++ src/server/routes/clients/pessoaFisicaRoute.ts
@@ -5,7 +5,9 @@ export const pessoaFisicaRouters = (router: Router) => {
   const routers = [
     router.post('/clientes/pessoaFisica', pessoaFisicaControllers.createValidation, pessoaFisicaControllers.create),
     router.put('/clientes/pessoaFisica/:id', pessoaFisicaControllers.updateByIdValidation, pessoaFisicaControllers.updateById),
-    router.get('/clientes/pessoaFisica/:id', pessoaFisicaControllers.getByIdValidation, pessoaFisicaControllers.getByID)
+    router.get('/clientes/pessoaFisica/:id', pessoaFisicaControllers.getByIdValidation, pessoaFisicaControllers.getByID),
+    router.get('/clientes/pessoaFisica', pessoaFisicaControllers.getAllValidation, pessoaFisicaControllers.getAll),
+    router.delete('/clientes/pessoaFisica/:id', pessoaFisicaControllers.deleteByIdValidation, pessoaFisicaControllers.deleteById)
   ];
 
   return {...routers};
