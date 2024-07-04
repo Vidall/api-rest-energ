8b300e6402555ce4169ec6ae090c25100484da36
luan vidal <luanvidal6565@outlook.com>
2024-07-04 12:04:15 -0300

    feat: Pegar por ID uma Pessoa Fisica - Controllers

--- src/server/controllers/clients/pessoaFisica/index.ts
+++ src/server/controllers/clients/pessoaFisica/index.ts
@@ -1,7 +1,9 @@
 import * as create from './Create';
 import * as UpdateById from './UpdateById';
+import * as getById from './GetById';
 
 export const pessoaFisicaControllers = {
   ...create,
-  ...UpdateById
+  ...UpdateById,
+  ...getById
 };
--- src/server/routes/clients/pessoaFisicaRoute.ts
+++ src/server/routes/clients/pessoaFisicaRoute.ts
@@ -4,7 +4,8 @@ import { pessoaFisicaControllers } from '../../controllers/clients/pessoaFisica'
 export const pessoaFisicaRouters = (router: Router) => {
   const routers = [
     router.post('/clientes/pessoaFisica', pessoaFisicaControllers.createValidation, pessoaFisicaControllers.create),
-    router.put('/clientes/pessoaFisica/:id', pessoaFisicaControllers.updateByIdValidation, pessoaFisicaControllers.updateById)
+    router.put('/clientes/pessoaFisica/:id', pessoaFisicaControllers.updateByIdValidation, pessoaFisicaControllers.updateById),
+    router.get('/clientes/pessoaFisica/:id', pessoaFisicaControllers.getByIdValidation, pessoaFisicaControllers.getByID)
   ];
 
   return {...routers};
