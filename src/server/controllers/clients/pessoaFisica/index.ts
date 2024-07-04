619db2c532521bf1f4848c4b20cb5b4b1a3b098f
luan vidal <luanvidal6565@outlook.com>
2024-07-04 16:58:15 -0300

    feat: controller - pegar todos cliente pessoa fisica

--- src/server/controllers/clients/pessoaFisica/index.ts
+++ src/server/controllers/clients/pessoaFisica/index.ts
@@ -1,9 +1,11 @@
 import * as create from './Create';
 import * as UpdateById from './UpdateById';
 import * as getById from './GetById';
+import * as getAll from './getAll';
 
 export const pessoaFisicaControllers = {
   ...create,
   ...UpdateById,
-  ...getById
+  ...getById,
+  ...getAll
 };
--- src/server/routes/clients/pessoaFisicaRoute.ts
+++ src/server/routes/clients/pessoaFisicaRoute.ts
@@ -5,7 +5,8 @@ export const pessoaFisicaRouters = (router: Router) => {
   const routers = [
     router.post('/clientes/pessoaFisica', pessoaFisicaControllers.createValidation, pessoaFisicaControllers.create),
     router.put('/clientes/pessoaFisica/:id', pessoaFisicaControllers.updateByIdValidation, pessoaFisicaControllers.updateById),
-    router.get('/clientes/pessoaFisica/:id', pessoaFisicaControllers.getByIdValidation, pessoaFisicaControllers.getByID)
+    router.get('/clientes/pessoaFisica/:id', pessoaFisicaControllers.getByIdValidation, pessoaFisicaControllers.getByID),
+    router.get('/clientes/pessoaFisica', pessoaFisicaControllers.getAllValidation, pessoaFisicaControllers.getAll)
   ];
 
   return {...routers};
