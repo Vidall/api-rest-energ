9c5aa63c1400e550537189a16a7da6cb3584b07d
luan vidal <luanvidal6565@outlook.com>
2024-07-04 17:13:36 -0300

    feat: created controller getAll pessoaFisica

---
+++ src/server/controllers/clients/pessoaFisica/getAll.ts
@@ -0,0 +1,33 @@
+import * as yup from 'yup';
+
+import { validation } from '../../../shared/middlewares/validation';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+interface IQueryProps {
+  id?: number,
+  page?: number,
+  limit?: number,
+  filter?: string
+}
+
+export const getAllValidation = validation((getSchema) => ({
+  query: getSchema<IQueryProps>(yup.object().shape({
+    page: yup.number().optional().moreThan(0),
+    limit: yup.number().optional().moreThan(0),
+    filter: yup.string().optional(),
+    id: yup.number().integer().optional().default(0)
+  }))
+}));
+
+export const getAll = async (req: Request<IQueryProps>, res: Response) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 7;
+  const filter = req.query.filter || '';
+  const id = Number(req.query.id) || 0;
+
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', 1);
+
+  return res.status(StatusCodes.OK).json({page, limit, filter, id});
+};
