42cfa9d4e643388afff89714bc964a9535a1b333
luan vidal <luanvidal6565@outlook.com>
2024-07-05 07:06:27 -0300

    test: configurando o ambiente jest

---
+++ tests/jest.setup.ts
@@ -0,0 +1,15 @@
+import supertest from 'supertest';
+
+import { server } from '../src/server/Server';
+import { knex } from '../src/server/database/knex';
+
+/*eslint-disable no-undef*/
+beforeAll(async () => {
+  await knex.migrate.latest();
+});
+
+afterAll(async () => {
+  await knex.destroy();
+});
+
+export const testServer = supertest(server);
